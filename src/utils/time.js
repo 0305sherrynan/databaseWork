70182d00ed363afa23ce76d467864269afd8132a
zheng <2642198391@qq.com>
2022-12-16 20:22:59 +0800

    feat(工具):工具文件：新增新时间格式化工具

--- src/utils/time.js
+++ src/utils/time.js
@@ -4,5 +4,15 @@ export const formateDate = ((time)=> {
       date = date.substring(0, 10)
     return date
   }
+)
 
+
+export const   timeTrans = (
+  (time)=>{
+    let date = new Date(new Date(time).getTime() + 8 * 3600 * 1000)
+    date = date.toJSON();
+      date = date.substring(0, 19).replace('T', ' ')
+    return date
+  
+  }
 )
