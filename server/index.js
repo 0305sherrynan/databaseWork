f2da4d33688e5df4b78b5efdfcf6e0776bed9e12
zheng <2642198391@qq.com>
2022-12-16 20:32:50 +0800

    feat：服务器入口文件:新增grade功能

--- server/index.js
+++ server/index.js
@@ -13,11 +13,13 @@ const course = require('./api/course.js')
 const student = require('./api/student.js')
 const dept = require('./api/dept.js')
 const teacher = require('./api/teacher.js')
+const grade = require('./api/grade.js')
 //使用接口
 app.use('/api/course',course)
 app.use('/api/student',student)
 app.use('/api/dept',dept)
 app.use('/api/teacher',teacher)
+app.use('/api/grade',grade)
 
 app.get('/zwz',(req,res)=>{
     res.send('当前服务器已连接')
