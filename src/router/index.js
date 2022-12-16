54e3190d2bd7e49dc78f34e786d168c44bfd54bf
zheng <2642198391@qq.com>
2022-12-16 20:31:13 +0800

    feat：路由入口config文件：新增多个界面

--- src/router/index.js
+++ src/router/index.js
@@ -20,8 +20,23 @@ const routes = [
     },
     {
         path:'/TeacherInsert',component:()=>import('@/pages/teacher/TeacherInsert.vue')
+    },
+    {
+        path:'/CourseOp',component:()=>import('@/pages/course/CourseOp.vue')
+    },
+    {
+        path:'/CourseInsert',component:()=>import('@/pages/course/CourseInsert.vue')
+    },
+    {
+        path:'/GradeOp',component:()=>import('@/pages/grade/GradeOp.vue')
+    },
+    {
+        path:'/GradeInsert',component:()=>import('@/pages/grade/GradeInsert.vue')
+    },
+    {
+        path:'/ReportForm',component:()=>import('@/pages/statistics/ReportForm.vue')
     }
-
+    
 ]
 const router = new VueRouter({routes})
 export default router
