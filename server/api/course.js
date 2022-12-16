e46318374aeef584ec9ad74c4fd2f2fe2f9d3604
zheng <2642198391@qq.com>
2022-12-16 20:20:18 +0800

    feat:服务器接口文件：course模块

--- server/api/course.js
+++ server/api/course.js
@@ -1,8 +1,43 @@
 const express = require('express')
 const  router = express.Router()
+const $sql = require('../dao/impl/courseMapImpl.js')
 
-router.get('/getInfo',(req,res)=>{
-    res.send({'zwz':'123'})
+
+/**
+ *  展示所有的course信息
+ */
+router.get('/findAllMenu',(req,res)=>{
+    // res.send({'zwz':'123'})
+    $sql.findAllMenu(req,res)
+})
+
+/**
+ *  增加course信息
+ */
+router.post('/addPerInfo',(req,res)=>{
+    // console.log(req.body)
+    $sql.addPerInfo(req,res)
+})
+
+/**
+ * 索引course的信息
+ */
+router.post('/findSpecialInfo',(req,res)=>{
+    $sql.findSpecialInfo(req,res)
+})
+
+/**
+ * 修改course的信息
+ */
+router.post('/modifyInfo',(req,res)=>{
+    $sql.modifyInfo(req,res)
+})
+
+/**
+ * 删除course的信息
+ */
+ router.post('/deleteInfo',(req,res)=>{
+    $sql.deleteInfo(req,res)
 })
 
 
