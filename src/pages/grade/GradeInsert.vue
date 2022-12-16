f9599b4cc3323ce55b30214b61f693a0ee30185c
zheng <2642198391@qq.com>
2022-12-16 20:24:07 +0800

    feat:路由界面:grade界面

---
+++ src/pages/grade/GradeInsert.vue
@@ -0,0 +1,76 @@
+
+<template>
+  <div>
+    <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
+
+    <el-dialog :append-to-body="true" title="收货地址" :visible.sync="dialogFormVisible" class="thisblack-bgc">
+            <el-form :model="form" class="flexForm">
+                <el-form-item label="学号" :label-width="formLabelWidth">
+                    <el-input v-model="form.sno" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="课程号" :label-width="formLabelWidth">
+                    <el-input v-model="form.cno" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="平时成绩" :label-width="formLabelWidth">
+                    <el-input v-model="form.often_score" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="考试成绩" :label-width="formLabelWidth">
+                    <el-input v-model="form.test_score" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="总评成绩" :label-width="formLabelWidth">
+                    <el-input v-model="form.total_score" autocomplete="off"></el-input>
+                </el-form-item>
+
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="makesureBtn">确 定</el-button>
+            </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import {insertGradeInfo} from '@/api/grade.js'
+export default {
+    name:'DeptInsert',
+    data(){
+        return {
+            dialogFormVisible: false,
+            form: {
+                sno:'',
+                cno:'',
+                often_score:'',
+                test_score:'',
+                total_score:''
+                },
+            formLabelWidth: '120px'
+        }
+    },
+    methods:{
+        async makesureBtn(){
+            // 
+            this.dialogFormVisible = false
+            console.log(this.form)
+            const data = await insertGradeInfo(this.form)
+            console.log(data)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.flexForm{
+    /* display: flex; */
+}
+::v-deep .el-dialog{
+    margin-top: 0 !important;
+    width: 500px ;
+    /* 居中 */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+}
+</style>
---
+++ src/pages/grade/GradeOp.vue
@@ -0,0 +1,280 @@
+<template>
+  <div class="studentOp">
+      <el-table
+          ref="tableInfo"
+          :data="tableDataNew"
+          border
+          height= "500"
+          style="width: 100%;">
+            <el-table-column   
+                fixed
+                key="2"
+                prop="sno"
+                label="学号"
+                width="120">
+            </el-table-column>
+            <el-table-column   
+                v-if="isShowCname('cname')"
+                key="1"
+                prop="cname"
+                label="课程名"
+                width="120">
+            </el-table-column>
+            <el-table-column   
+                v-if="isShowCname('sname')"
+                key="8"
+                prop="sname"
+                label="姓名"
+                width="120">
+            </el-table-column>
+            <el-table-column   
+                v-if="isShowCname('tname')"
+                key="9"
+                prop="tname"
+                label="教师姓名"
+                width="120">
+            </el-table-column>
+            <el-table-column   
+                v-if="isShowCname('sdept')"
+                key="10"
+                prop="sdept"
+                label="系号"
+                width="120">
+            </el-table-column>
+            <el-table-column
+                key="3"
+                prop="cno"
+                label="课程号"
+                width="120">
+            </el-table-column>
+            <el-table-column
+              key="4"
+              prop="often_score"
+              label="平时成绩"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              key="5"
+              prop="test_score"
+              label="考试成绩"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              key="6"
+              prop="total_score"
+              label="总评成绩"
+              width="120">
+            </el-table-column>
+            <el-table-column
+              key="7"
+              fixed="right"
+              label="操作"
+              width="200"
+              style="display:flex">
+              <template slot="header" slot-scope="scope" >
+                <el-select v-model="select" placeholder="请选择查询" width="80">
+
+                      <el-option label="学号" value="sno"></el-option>
+                      <el-option label="学生姓名" value="sname"></el-option>
+                      <el-option label="课程号" value="cno"></el-option>
+                      <el-option label="课程名称" value="cname"></el-option>
+                      <el-option label="上课教师姓名" value="tname"></el-option>
+                      <el-option label="系号" value="sdept"></el-option>
+                      <!-- <el-option label="总评成绩" value="total_score"></el-option> -->
+                </el-select>
+                <el-input
+                  name='search'
+                  width="80"
+                  v-model="search"
+                  size="mini"
+                  @keyup.enter.native="searchData"
+                  placeholder="索引"/>
+              </template>
+              <template slot-scope="scope">
+                <el-button @click="deleInfo(scope.row)" type="text" size="small">删除</el-button>
+                <el-button type="text" size="small" @click="editInfo(scope.row)">编辑</el-button>
+
+              </template>
+            </el-table-column>
+      </el-table>
+      <el-dialog :append-to-body="true" title="编辑成绩" :visible.sync="dialogFormVisible" class="thisblack-bgc">
+          <el-form :model="singalInfo" class="flexForm">
+              <el-form-item label="学号" :label-width="formLabelWidth">
+                  <el-input v-model="singalInfo.sno" :placeholder="singalInfo.sno" autocomplete="off" class="popWinInput"></el-input>
+              </el-form-item>
+              <el-form-item label="课程号" :label-width="formLabelWidth">
+                  <el-input v-model="singalInfo.cno" :placeholder="singalInfo.sname" autocomplete="off" class="popWinInput"></el-input>
+              </el-form-item>
+              <el-form-item label="平时成绩" :label-width="formLabelWidth">
+                  <el-input v-model="singalInfo.often_score" autocomplete="off" class="popWinInput"></el-input>
+              </el-form-item>
+              <el-form-item label="考试成绩" :label-width="formLabelWidth">
+                  <el-input v-model="singalInfo.test_score" autocomplete="off" class="popWinInput"></el-input>
+              </el-form-item>
+              <el-form-item label="总评成绩" :label-width="formLabelWidth">
+                  <el-input v-model="singalInfo.total_score" autocomplete="off" class="popWinInput"></el-input>
+              </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+              <el-button type="primary" @click="makesureBtn">确 定</el-button>
+          </div>
+      </el-dialog>
+  </div>
+</template>
+
+<script>
+// import {getDeptInfo,findSpecialInfo,modifyInfo,deleteInfo} from '@/api/dept.js'
+import {getGradeInfo,deleteInfo,modifyInfo,findSpecialInfo} from '@/api/grade.js'
+import {formateDate} from '@/utils/time.js'
+export default {
+  name:'GradeOp',
+  data(){
+    return {
+      //索引
+      search:'',
+      //索引对象
+      select:'',
+      
+      tableDataNew:[
+            
+        ],
+
+      //dialog
+      dialogFormVisible:false,
+      //form-item的大小
+      formLabelWidth: '120px',
+      //单行数据
+      singalInfo:{},
+      uid:'',
+      uid2:''
+    }
+  },
+  async created(){
+    //一开始获得表格数据
+    await this.getAgainTableData()
+    // console.log(this.tableDataNew.cname)
+
+  },
+  methods: {
+      //（重新）获取表格数据并将事件进行格式化
+      async getAgainTableData(){
+        const {data:res} = await getGradeInfo()
+        this.tableDataNew =   res.result.result
+        // this.timeFilter()
+      },
+      //时间格式的过滤
+      timeFilter(){
+        console.log(this.tableDataNew)
+        this.tableDataNew.forEach((value)=>{
+          console.log(value)
+          value.sbirthday = formateDate(value.sbirthday)
+        })
+        
+      },
+      handleClick(row) {
+        console.log(row)
+
+      },
+      //索引数据
+      async searchData(){
+        console.log(this.select,this.search)
+        const {data:res} = await findSpecialInfo(this.select,this.search)
+        console.log(res)
+        this.tableDataNew = await res.result.result
+        // this[(this.select==='cname'?'isShowCname':'')]()
+        // this.isShowCname(this.select)
+        
+        // this.timeFilter()
+                this.$nextTick(()=>{
+          this.$refs.tableInfo.doLayout()
+        })
+        
+      },
+      /**
+       * 编辑数据
+       */
+      async editInfo(row){
+        //标识后面会修改的信息
+        this.uid =row.sno
+        this.uid2 = row.cno
+        this.singalInfo = await row
+        console.log(this.singalInfo)
+        this.dialogFormVisible = true
+      },
+      //是否显示cname
+      isShowCname(select){
+        
+        if (this.tableDataNew[select] == undefined) return false
+        // console.log( Object.hasOwn(this.tableDataNew[0], 'cname'))
+        return Object.hasOwn(this.tableDataNew[0], select)
+      },
+      // isShowSname(){
+      //   return Object.hasOwn(this.tableDataNew[0], 'sname')
+      // },
+      //提交数据
+      async makesureBtn(){
+          const id = this.uid
+          const id2 = this.uid2
+          const {data:res} = await modifyInfo({id,id2,...this.singalInfo})
+          console.log(res)
+      },
+      //删除数据
+      async deleInfo(row){
+         await deleteInfo(row.sno,row.cno)
+         //重新获得数据
+          this.getAgainTableData()
+
+         
+  
+      }
+        //       tableRowClassName({row, rowIndex}) {   //设置table的样式
+            
+        // if (rowIndex%2 === 0) {
+        //     // console.log(rowIndex)
+        //   return 'warning-row';
+        // } else if (rowIndex%2 === 1) {
+        //   return 'success-row';
+        // }
+        // return '';
+        // },
+    }
+
+}
+</script>
+
+<style scoped>
+.studentOp{
+    width: 100%;
+    height: 100%;
+}
+/* @import "../assets/css/table_text.css"; */
+/* ::v-deep .table{
+    background: #e2d2d2;
+} */
+::v-deep .cell{
+  display: flex !important;
+
+}
+::v-deep .el-input__inner{
+  width: 90px;
+  height: 40px;
+}
+
+
+::v-deep .popWinInput .el-input__inner{
+  width: 100%;
+}
+
+::v-deep .el-dialog{
+    margin-top: 0 !important;
+    width: 500px ;
+    /* 居中 */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    box-shadow:none
+
+}
+</style>
